1d08cdf13964804f609aa0ec0036277917ab6558
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2023-11-06 22:19:05 +0330

    u

--- login-signup/src/component/LoginSignup/LoginSignup.jsx
+++ login-signup/src/component/LoginSignup/LoginSignup.jsx
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
 import './LoginSignup.css'
-
 import user_icon from '../Assets/person.png'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 
 const LoginSignup = () => {
-
     const [action, setAction] = useState("Sign Up");
-
     return (
         <div className="container">
             <div className="header">
@@ -39,5 +36,4 @@ const LoginSignup = () => {
         </div>
     )
 }
-
 export default LoginSignup
