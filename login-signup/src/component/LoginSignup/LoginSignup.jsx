62caf0573bf50ec2f7e8b0dd6ea0bcfa1357cecf
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2023-11-27 23:31:37 +0330

    Update LoginSignup.jsx

--- login-signup/src/component/LoginSignup/LoginSignup.jsx
+++ login-signup/src/component/LoginSignup/LoginSignup.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import './LoginSignup.css'
-
 import user_icon from '../Assets/person.png'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
@@ -16,11 +15,11 @@ const LoginSignup = () => {
                 <div className="underline"></div>
             </div>
             <div className="inputs">
-                {action==="Login"?<div></div>:
-                <div className="input">
-                    <img src={user_icon} alt="" />
-                    <input type="text" placeholder="Name" />
-                </div>}
+                {action === "Login" ? <div></div> :
+                    <div className="input">
+                        <img src={user_icon} alt="" />
+                        <input type="text" placeholder="Name" />
+                    </div>}
                 <div className="input">
                     <img src={email_icon} alt="" />
                     <input type="email" placeholder="Email id" />
@@ -30,8 +29,8 @@ const LoginSignup = () => {
                     <input type="password" placeholder="Password" />
                 </div>
             </div>
-            {action==="Sign Up"?<div></div>:
-            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
+            {action === "Sign Up" ? <div></div> :
+                <div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
             <div className="submit-container">
                 <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Sign Up") }}>Sign Up</div>
                 <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
